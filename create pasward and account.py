a06c6b1138bf5952698ad302885fe24f9b91d982
maxdunn7 <37370737+maxdunn7@users.noreply.github.com>
2018-03-14 12:38:35 +0000

    create password and username

---
+++ create pasward and account.py
@@ -0,0 +1,28 @@
+
+testScore = int(input('Enter test score: '))
+if testScore >100:
+    print("error")
+testScore = int(input('Enter test score: '))
+if testScore <= 100:
+ print ("A*")   
+testScore = int(input('Enter test score: '))
+if testScore <= 90:
+        print('Your grade is A')
+elif testScore <= 80:
+        print('Your grade is B')
+elif testScore <= 70:
+        print('Your grade is C')
+elif testScore <= 60:
+        print('Your grade is D')
+else:
+        print('Your grade is F')
+ 
+
+
+
+        
+        
+        
+     
+
+
